7720236d7f42d41ce63e8e81d41d938f6a678934
Ivan <senko.ivan999@mail.ru>
2023-01-20 08:45:17 +0300

    Задача_1

--- task_1.java
+++ task_1.java
@@ -1,17 +1,36 @@
 package Main;
+import java.util.Arrays;
 import java.util.Random;
 
 public class task_1 {
     public static void main(String[] args) {
-        int i = new Random().nextInt(999999);
+        int i = new Random().nextInt(99999);
         int n = Integer.toBinaryString(i).length();
 
-        System.out.println(i);
-        System.out.println(n);
+        int c =0;
+        for (int j = Short.MIN_VALUE; j < i; j++) {
+            if (j%n == 0) c++;
         
+        }
+
+    int[] a1 = new int[c];
+    c = 0;
+    for (int j = Short.MIN_VALUE; j < i; j++) {
+        if (j%n == 0) a1[c++] = j;
     }
 
+    c = 0;
+    for (int j = i; j < Short.MAX_VALUE; j++) {
+        if (j%n != 0) c++;
+    }
+
+    int[] a2 = new int[c];
+    c = 0;
+    for (int j = i; j < Short.MIN_VALUE; j++) {
+        if (j%n != 0) a2[c++] = j;
+    }
     
-}
+    System.out.println(Arrays.toString(a1));
+}}
 
     
